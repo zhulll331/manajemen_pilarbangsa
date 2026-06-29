db753cc741557c627df06357b28d82ef422860cd
Zhull Dev <zhull3792@gmail.com>
2026-06-29 22:14:30 +0700

    chore: force Eruda initialization beforeInteractive in HTML head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,20 +29,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script src="https://cdn.jsdelivr.net/npm/eruda" strategy="beforeInteractive" />
+        <Script id="eruda-init" strategy="beforeInteractive">
+          {`if (typeof eruda !== "undefined") eruda.init();`}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
         <SplashScreen />
         {children}
-        <Script
-          src="https://cdn.jsdelivr.net/npm/eruda"
-          strategy="afterInteractive"
-          onLoad={() => {
-            // @ts-ignore
-            if (typeof window !== 'undefined' && window.eruda) {
-              // @ts-ignore
-              window.eruda.init();
-            }
-          }}
-        />
       </body>
     </html>
   );
