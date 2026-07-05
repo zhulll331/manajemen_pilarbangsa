f2ce89b9ed4679091df2cacbc41865314f93ecb5
Zhull Dev <zhull3792@gmail.com>
2026-07-05 20:55:07 +0700

    chore: increase bodySizeLimit to 15mb for file uploads

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '4mb',
+      bodySizeLimit: '15mb',
     },
   },
 };
